e4cf92b1d797b6592f9a75a362cf86002052128f
adiyaturkpen <a.turkpen@gmail.com>
2025-06-11 13:48:32 +0500

    2

--- lab4/json/t1_j.py
+++ lab4/json/t1_j.py
@@ -1,7 +1,7 @@
 import json
-fi = open(r"C:\Users\Lenovo\pp2\pp2-summer\lab4\json\sample-data.json")
-data = json.load(fi)
-moredata= data["imdata"]
+fi=open(r"C:\Users\Lenovo\pp2\pp2-summer\lab4\json\sample-data.json")
+data=json.load(fi)
+moredata=data["imdata"]
 print("Interface Status" "\n"
 "================================================================================" "\n"
 "DN                                                 Description           Speed    MTU  " "\n"
