abd423a2ce23c37433287e5d8cfd3393ea6d219f
adiyaturkpen <a.turkpen@gmail.com>
2025-06-11 13:23:54 +0500

    poluchilos

--- lab4/json/t1_j.py
+++ lab4/json/t1_j.py
@@ -1,3 +1,11 @@
 import json
-with open("sample-data.json") as f:
-    data = json.load(f)
+fi = open(r"C:\Users\Lenovo\pp2\pp2-summer\lab4\json\sample-data.json")
+data = json.load(fi)
+moredata= data["imdata"]
+print("Interface Status" "\n"
+"================================================================================" "\n"
+"DN                                                 Description           Speed    MTU  " "\n"
+"-------------------------------------------------- --------------------  ------  ------" "\n")
+
+for item in moredata:
+    print(item["l1PhysIf"]["attributes"]["dn"],'                            ',item["l1PhysIf"]["attributes"]["speed"],' ',item["l1PhysIf"]["attributes"]["mtu"])
