5e654cc4c2d8216535cf11a732cba3f16b97bf5e
adiyaturkpen <a.turkpen@gmail.com>
2025-06-13 16:19:10 +0500

    3

--- lab5/t10_r.py
+++ lab5/t10_r.py
@@ -2,5 +2,5 @@ import re
 fi=open(r"C:\Users\Lenovo\pp2\pp2-summer\lab5\row.txt", encoding="utf-8")
 data=fi.read()
 fi.close()
-m=re.sub(' ','_', data).lower()
+m=re.sub(r"[A-Z]",'_',data)
 print(m)
--- lab5/t9_r.py
+++ lab5/t9_r.py
@@ -2,5 +2,5 @@ import re
 fi=open(r"C:\Users\Lenovo\pp2\pp2-summer\lab5\row.txt", encoding="utf-8")
 data=fi.read()
 fi.close()
-m=re.sub(r'([A-Z])', r' \1', data)
+m=re.sub(r'([A-Z])', ' ', data)
 print(m)
